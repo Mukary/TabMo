ee067311cd7c2504bbf08e9cd3c658a2df8f3668
jhebmann <julien.hebmann@gmx.fr>
2017-10-20 22:50:43 +0200

    Better csv generation

--- pre-process.scala
+++ pre-process.scala
@@ -1,6 +1,4 @@
 //Converting JSON file to CSV with headers - by Julien Hebmann
 
  val data = spark.read.json("data-students.json")
- val stringify = udf((vs: Seq[String]) => s"""[${vs.mkString(",")}]""")
-
- data.withColumn("size", stringify($"size")).coalesce(1).write.option("header","true").csv("./sample-data.csv") 
+ data.withColumn("size", concat(lit("["), concat_ws(",",$"size"),lit("]"))).coalesce(1).write.option("header","true").csv("data-students")
