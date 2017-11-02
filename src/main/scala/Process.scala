d8831d2b560e84d323eb94a781fc1b24f2115420
maxcabourg <max.cabourg@etu.umontpellier.fr>
2017-11-02 16:10:09 +0100

    <add> Rename timestamp column in period + config to run on stand-alone

--- src/main/scala/Process.scala
+++ src/main/scala/Process.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types.DateType
 import org.apache.spark.sql.types.TimestampType
@@ -6,12 +7,18 @@ import org.apache.spark.sql.functions._
 
 object Process{
    def main(args: Array[String]){
-    val spark = SparkSession.builder.appName("Web Intelligence").getOrCreate()
+    val conf = new SparkConf().setMaster("local[2]")
+    val spark = SparkSession.builder.appName("Web Intelligence").config(conf).getOrCreate()
+
     import spark.implicits._
     
-    val sourceJson = spark.read.json("/home/data/data-students.json")
-    val cleansedJson = sourceJson.drop("network").drop("impid").withColumn("timestamp", sourceJson("timestamp").cast(TimestampType).cast(DateType)).withColumn("os", lower($"os"))
+    val sourceJson = spark.read.json("/home/maxcabourg/Documents/Polytech/WI/data-students.json")
+    val cleansedJson = sourceJson.drop("network")
+      .drop("impid")
+      .withColumn("timestamp", sourceJson("timestamp").cast(TimestampType).cast(DateType))
+      .withColumn("os", lower($"os"))
+      .withColumnRenamed("timestamp", "period")
     
-    cleansedJson.withColumn("size", concat(lit("["), concat_ws(",",$"size"),lit("]"))).coalesce(1).write.option("header","true").csv("data-csv")
+    cleansedJson.withColumn("size", concat(lit("["), concat_ws(",",$"size"),lit("]"))).coalesce(1).write.option("header","true").csv("/home/maxcabourg/Documents/Polytech/WI/data-csv")
    }
 }
