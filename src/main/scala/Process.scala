f8077b9fb080253002459183dc7dfce706a914f0
loicfaivre <themonheal@gmail.com>
2017-11-03 19:51:56 +0100

    Feat<Pre-Process> : Add comment for timestamp processing function

--- src/main/scala/Process.scala
+++ src/main/scala/Process.scala
@@ -24,7 +24,11 @@ object Process{
     val conf = new SparkConf().setMaster("local[2]")
     val spark = SparkSession.builder.appName("Web Intelligence").config(conf).getOrCreate()
 
-
+    /**
+    * Convert the value of the hour in input to one the strings : "Night" (between 6pm and 5am), "Morning" (between 6am and 11am) and "Afternoon" (between 12pm and 5pm) 
+    * @param arg : one value from the column timestamp (one hour)
+    * @return the correct period regarding the hour in input
+    */
     val coder: (Int => String) = (arg: Int) => {if (((18 <= arg) && (arg <= 23)) || ((0 <= arg) && (arg <= 5))) "Night" else if ((6 < arg) && (arg < 11)) "Afternoon" else "Morning"}
     val timestampToConvert = udf(coder)
 
