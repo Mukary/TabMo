1dca3317d0cdce1a10e151a0ff3dd80f8527ac8e
maxcabourg <max.cabourg@etu.umontpellier.fr>
2017-11-03 12:19:35 +0100

    <add> First draft with logistic regression (only bidfloor feature)

--- src/main/scala/TabmoLogisticRegression.scala
+++ src/main/scala/TabmoLogisticRegression.scala
@@ -4,29 +4,28 @@ import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 import TabmoEncoder._
+import org.apache.spark.sql.types.DoubleType
 
 object TabmoLogisticRegression{
   def bool2double(b: Boolean) = if (b) 1f else 0f
 
   def main(args: Array[String]){
-    val sc = new SparkContext()
-    val spark = SparkSession.builder.appName("Web Intelligence").getOrCreate()
+    val conf = new SparkConf().setMaster("local[2]").setAppName("Web Intelligence")
+    val sc = new SparkContext(conf)
+    val spark = SparkSession.builder.appName("Web Intelligence").config(conf).getOrCreate()
+    sc.setLogLevel("ERROR")
     //val data = MLUtils.loadLibSVMFile(sc, args(0))
     
     val rawData = TabmoEncoder.getVectorizedData(args(0))
-    val labelIndex = rawData.columns.indexOf("label")
-    
+    rawData.printSchema()
+    val labelIndex = rawData.columns.indexOf("labelIndex")
+
     //TODO: fix ClassCastException: java.lang.String cannot be cast to java.lang.Boolean
     val data = rawData.rdd.map(row => {
        val doubleArray = row.toSeq.toArray
-       LabeledPoint(bool2double(row.getBoolean(labelIndex)), Vectors.dense(doubleArray.map(e => {
-         if(e.isInstanceOf[Boolean])
-           bool2double(e.asInstanceOf[Boolean])
-         else
-          e.asInstanceOf[Double]
-       })))
+       LabeledPoint(row.getDouble(labelIndex), Vectors.dense(Array(row.getString(0).toDouble)))
      })
 
     // Split data into training (60%) and test (40%).
@@ -36,7 +35,7 @@ object TabmoLogisticRegression{
 
     // Run training algorithm to build the model
     val model = new LogisticRegressionWithLBFGS()
-      .setNumClasses(10)
+      .setNumClasses(2)
       .run(training)
 
     // Compute raw scores on the test set.
@@ -48,10 +47,13 @@ object TabmoLogisticRegression{
     // Get evaluation metrics.
     val metrics = new MulticlassMetrics(predictionAndLabels)
     val accuracy = metrics.accuracy
+    val confusionMatrix = metrics.confusionMatrix
     println(s"Accuracy = $accuracy")
+    println(s"Confusion matrix = $confusionMatrix")
+
 
     // Save and load model
-    model.save(sc, "target/tmp/scalaLogisticRegressionWithLBFGSModel")
-    val sameModel = LogisticRegressionModel.load(sc, "target/tmp/scalaLogisticRegressionWithLBFGSModel")
+    /*model.save(sc, "/home/maxcabourg/Documents/Polytech/tmp/scalaLogisticRegressionWithLBFGSModel")
+    val sameModel = LogisticRegressionModel.load(sc, "target/tmp/scalaLogisticRegressionWithLBFGSModel")*/
   } 
 }
