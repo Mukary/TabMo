75e2383d730ceff922fc333123be930e9ba03980
maxcabourg <max.cabourg@etu.umontpellier.fr>
2017-11-03 15:07:10 +0100

    <add> Fuse features in one column to feed it to the ML algorithm

--- src/main/scala/TabmoLogisticRegression.scala
+++ src/main/scala/TabmoLogisticRegression.scala
@@ -1,11 +1,12 @@
 import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticRegressionWithLBFGS}
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
-import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.linalg.{SparseVector, Vector, Vectors}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 import TabmoEncoder._
+import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.types.DoubleType
 
 object TabmoLogisticRegression{
@@ -19,17 +20,24 @@ object TabmoLogisticRegression{
     //val data = MLUtils.loadLibSVMFile(sc, args(0))
     
     val rawData = TabmoEncoder.getVectorizedData(args(0))
-    rawData.printSchema()
+
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("bidfloor", "appOrSiteVec", "exchangeVec", "mediaVec", "osVec", "publisherVec", "sizeVec", "periodVec", "typeVec", "interestsVec"))
+      .setOutputCol("features")
+
+    val output = assembler.transform(rawData.withColumn("bidfloor", rawData("bidfloor").cast(DoubleType)))
+    output.printSchema
+    val mllibOutput = MLUtils.convertVectorColumnsFromML(output)
     val labelIndex = rawData.columns.indexOf("labelIndex")
 
+
     //TODO: fix ClassCastException: java.lang.String cannot be cast to java.lang.Boolean
-    val data = rawData.rdd.map(row => {
-       val doubleArray = row.toSeq.toArray
-       LabeledPoint(row.getDouble(labelIndex), Vectors.dense(Array(row.getString(0).toDouble)))
+    val data = mllibOutput.rdd.map(row => {
+       LabeledPoint(row.getDouble(labelIndex), row.getAs("features"))
      })
 
     // Split data into training (60%) and test (40%).
-    val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
+    val splits = data.randomSplit(Array(0.8, 0.2), seed = 11L)
     val training = splits(0).cache()
     val test = splits(1)
 
@@ -48,8 +56,22 @@ object TabmoLogisticRegression{
     val metrics = new MulticlassMetrics(predictionAndLabels)
     val accuracy = metrics.accuracy
     val confusionMatrix = metrics.confusionMatrix
+    val tp = confusionMatrix(0 ,0)
+    val fp = confusionMatrix(0 ,1)
+    val tn = confusionMatrix(1, 1)
+    val fn = confusionMatrix(1, 0)
+
+    val recall = tp / (tp + fn)
+    val precision = tp / (tp + fp)
     println(s"Accuracy = $accuracy")
-    println(s"Confusion matrix = $confusionMatrix")
+    println(s"TP = $tp")
+    println(s"TN = $tn")
+    println(s"FN = $fn")
+    println(s"fp = $fp")
+    println(s"recall = $recall")
+    println(s"precision = $precision")
+    println("Confusion matrix = ")
+    println(s"$confusionMatrix")
 
 
     // Save and load model
